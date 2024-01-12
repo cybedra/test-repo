a9891fd27e267eea5e69836d583323bd53a2b1a1
cybedra <147957633+cybedra@users.noreply.github.com>
2024-01-12 13:06:42 -0800

    closed #1

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 I am attempting to learn how to use GitHub
 
 Now I am editing the readme in VSCode
+
+I am editing multiple files at the same time
--- main.py
+++ main.py
@@ -1,4 +1,13 @@
+import sys
+
+def print_sys_props():
+    print(sys.version)
+    print(sys.executable)
+    print(sys.path)
+    
+
 def main():
-    print ("Hello World!");
+    print("Hello World!")
+    print_sys_props()
     
 main()
